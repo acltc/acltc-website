3329322282cfe1e070d1e6172aca877cdb58e36e
markeban <markeban@yahoo.com>
2015-07-10 10:22:05 -0500

    fixed admin edit and added bio to strong params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,10 +4,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:admin).permit(:first_name, :last_name, :title, :email, :avatar, :avatar_cache, :password, :password_confirmation)
+    params.require(:admin).permit(:first_name, :last_name, :title, :email, :avatar, :bio, :avatar_cache, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:admin).permit(:first_name, :last_name, :title, :email, :avatar, :avatar_cache, :password, :password_confirmation, :current_password)
+    params.require(:admin).permit(:first_name, :last_name, :title, :email, :avatar, :bio, :avatar_cache, :password, :password_confirmation, :current_password)
   end
 end
