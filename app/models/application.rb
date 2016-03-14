1b2cc1f065404edaf851238f8931c74d5ee25a43
rymcmahon <toedragdangle9@gmail.com>
2016-03-14 11:00:08 -0500

    added radio buttons for new application form location and scholarship

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,6 @@
 class Application < ActiveRecord::Base
   has_and_belongs_to_many :locations
+  validates :legal_name, :email, :phone, :address, :emergency_contact, :learn_about_acltc, :current_occupation, :locations, :primary_goals, :programming_experience, :preferred_work_location, :capstone_idea, :work_concurrently, :tinker_example, :why_better,  presence: true
 
   def full_name
     "#{first_name} #{last_name}"
