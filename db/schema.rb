cfad36a4c34c90d99e66b9531bf4af2c60ba21ec
rymcmahon <toedragdangle9@gmail.com>
2016-04-01 06:06:17 -0500

    added cohort select dropdown to applications show

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329202348) do
+ActiveRecord::Schema.define(version: 20160401015610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20160329202348) do
     t.string   "location"
     t.text     "learn_about_acltc_specify"
     t.string   "status"
+    t.string   "cohort"
   end
 
   create_table "blog_categories", force: :cascade do |t|
