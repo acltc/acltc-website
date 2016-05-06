9a4786002ba20e86975ffef7b8cea0dd4349bfbc
rymcmahon <toedragdangle9@gmail.com>
2016-05-05 21:07:37 -0500

    added conditional validation to application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,7 +1,9 @@
 class Application < ActiveRecord::Base
-  # validates :first_name, :last_name, :email, :phone, :address, :emergency_contact, :learn_about_acltc,
-  # :current_occupation, :location, :primary_goals, :programming_experience, :preferred_work_location,
-  # :work_concurrently, :tinker_example, :why_better, :interview_id, :hangouts_email,  presence: true, on: :create
+  validates :first_name, :last_name, :email, :phone, :address, :emergency_contact, :learn_about_acltc,
+  :current_occupation, :location, :tinker_example, :why_better, :interview_id, :hangouts_email, presence: true, on: :create
+
+  # validates :primary_goals, :programming_experience, :preferred_work_location, :work_concurrently, presence: true, if: "@form_page == 'long'"
+
   attr_accessor :subtitle #virtual attribute to prevent spam via the invisible_captcha gem
   validates :subtitle, :invisible_captcha => true
 
