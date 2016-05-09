82dd203ccf1dbf644720f216d32a09027fa9a0f6
rymcmahon <toedragdangle9@gmail.com>
2016-05-09 14:13:09 -0500

    decided on the short form questions for ab test

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,9 +1,9 @@
 class Application < ActiveRecord::Base
   validates :first_name, :last_name, :email, :phone, :address, :emergency_contact, :learn_about_acltc,
-  :current_occupation, :location, :primary_goals, :programming_experience, :hangouts_email, presence: true, on: :create
+  :location, :primary_goals, :programming_experience, :tinker_example, :hangouts_email, presence: true, on: :create
   validates :interview_id, presence: true, uniqueness: true, on: :create
 
-  validates :preferred_work_location, :work_concurrently, :tinker_example, :why_better, presence: true, on: :create, unless: Proc.new { |m| m.hidden == 'short' }
+  validates :current_occupation, :preferred_work_location, :work_concurrently, :why_better, presence: true, on: :create, unless: Proc.new { |m| m.hidden == 'short' }
 
   attr_accessor :hidden
   attr_accessor :subtitle #virtual attribute to prevent spam via the invisible_captcha gem
@@ -17,14 +17,6 @@ class Application < ActiveRecord::Base
 
   def applications_human_time
     updated_at.strftime("%A, %d %b %Y %l:%M %p")
-  end
-
-  def long_form
-    @form_page == 'long'
-  end
-
-  def short_form
-    @form_page == 'short'
   end
 
   def self.to_csv(options = {})
