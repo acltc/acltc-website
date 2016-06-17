a261c69e763f91f94c2de135b0d2e95c14d7da54
Peter Jang <peter.x.jang@gmail.com>
2016-06-17 11:21:28 -0500

    content for oop lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,7 +2,8 @@ class LessonsController < ApplicationController
   CATEGORIES = [
     { title: "Season 1: Becoming a Power User", icon: "icon-laptop2" },
     { title: "Season 2: Preparing Your Tools", icon: "icon-wrench" },
-    { title: "Season 3: Becoming a Programmer", icon: "icon-keyboard" }
+    { title: "Season 3: Becoming a Programmer", icon: "icon-keyboard" },
+    { title: "Season 4: Object Oriented Programming", icon: "icon-keyboard" }
   ].freeze
   
   LESSONS = [
@@ -54,7 +55,21 @@ class LessonsController < ApplicationController
     { title: "Episode 20 - Hashes As Objects", time: "8:38", category_id: 2},
     { title: "Episode 20a - Arrays of Hashes", time: "10:19", category_id: 2},
     { title: "Episode 21 - RubyGems", time: "5:19", category_id: 2},
-    { title: "Episode 22 - API Gems", time: "12:18", category_id: 2}
+    { title: "Episode 22 - API Gems", time: "12:18", category_id: 2},
+
+    { title: "Episode 1 - Intro to OOP", time: "10:04", category_id: 3},
+    { title: "Episode 2 - Arguments", time: "8:11", category_id: 3},
+    { title: "Episode 2a - Multiple Arguments", time: "3:56", category_id: 3},
+    { title: "Episode 2b - Methods: Things That Can Go Wrong", time: "5:44", category_id: 3},
+    { title: "Episode 3 - Side Effects", time: "4:46", category_id: 3},
+    { title: "Episode 4 - Instance Variables", time: "8:06", category_id: 3},
+    { title: "Episode 4a - More About Return Values", time: "3:54", category_id: 3},
+    { title: "Episode 4b - Idiomatic Ruby", time: "5:12", category_id: 3},
+    { title: "Episode 5 - Constructors", time: "5:53", category_id: 3},
+    { title: "Episode 6 - Methods Calling Methods", time: "3:28", category_id: 3},
+    { title: "Episode 7 - Synthesis: Putting It All Together", time: "11:35", category_id: 3},
+    { title: "Episode 8 - Converting Hashes To Objects", time: "7:41", category_id: 3}
+
   ].freeze
   
   def index
