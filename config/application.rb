08d6907f25b0f576a6ef07e79f0651b862392598
Seth Quittner <sethaugustus@gmail.com>
2016-07-29 10:34:22 -0700

    changed application.rb to try to fix font load omn heroku

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module AcltcWebsite
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.precompile += %w( home.css )
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join('/app/assets/fonts')
   end
 end
