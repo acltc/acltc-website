2ef6bfa0e4c7bae91c4f9813816530ca50d9c8c3
Seth Quittner <sethaugustus@gmail.com>
2016-08-02 08:02:17 -0700

    added pop up message modal window to home,

---
+++ app/assets/javascripts/disappear.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+
+      $('.disappear').click(function(){
+        $(".message").css("visibility", "hidden");
+        $(".marquee .marquee-content").css("padding", "7em 3.5em");
+       });
+
+});
