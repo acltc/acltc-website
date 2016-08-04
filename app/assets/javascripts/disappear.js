de733e995ba5ce02584286159e90b840f30c3417
Seth Quittner <sethaugustus@gmail.com>
2016-08-04 16:50:42 -0700

    added new message box text and 1st pass at RWD

--- app/assets/javascripts/disappear.js
+++ app/assets/javascripts/disappear.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
       $('.disappear').click(function(){
         $(".message").css("visibility", "hidden");
         $(".marquee .marquee-content").css("padding", "7em 3.5em");
+        $(".marquee").css("height", "648px");
+        $(".marquee").css("margin-top", "-232px");
        });
-
 });
