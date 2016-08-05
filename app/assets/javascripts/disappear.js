9b050a238057d4a10129e1a028ea68563daf1149
Seth Quittner <sethaugustus@gmail.com>
2016-08-05 07:31:42 -0700

    added jquery logic for media queries for message box

--- app/assets/javascripts/disappear.js
+++ app/assets/javascripts/disappear.js
@@ -5,5 +5,22 @@ $(document).ready(function(){
         $(".marquee .marquee-content").css("padding", "7em 3.5em");
         $(".marquee").css("height", "648px");
         $(".marquee").css("margin-top", "-232px");
+        if (window.matchMedia('(max-width: 320px)').matches)
+        {
+            $(".marquee .marquee-content").css("padding", "2em 3.5em");
+            $(".marquee .marquee-content").css("margin-top", "52px");
+            $(".marquee .marquee-content").css("height", "596px");
+        }
+        else if (window.matchMedia('(max-width: 480px)').matches)
+        {
+            $(".marquee .marquee-content").css("padding", "1em 3.5em");
+            $(".marquee .marquee-content").css("margin-top", "52px");
+            $(".marquee .marquee-content").css("height", "596px");
+        }
+        else if (window.matchMedia('(max-width: 768px)').matches)
+        {
+            $(".marquee .marquee-content").css("margin-top", "92px");
+            $(".marquee .marquee-content").css("height", "557px");
+        }
        });
 });
