11992fea6fe4b4ddf0b4de5674bfbade6dbd8790
dianacamacho <dcam1901@yahoo.com>
2016-09-06 15:31:51 -0500

    testing image src tag for email logo

--- app/mailers/acltc_mailer.rb
+++ app/mailers/acltc_mailer.rb
@@ -13,7 +13,7 @@ class AcltcMailer < ApplicationMailer
 
   def corporate_training_message_reply(corporate_training_message)
     @corporate_training_message = corporate_training_message
-    mail(from: @corporate_training_message.email, to: "theonetrueemailtest@gmail.com", subject: 'Your Corporate Training Message To ACLTC')
+    mail(from: "hello@anyonecanlearntocode.com", to: @corporate_training_message.email, subject: 'Your Corporate Training Message To ACLTC')
   end
 
   def application_email(application)
