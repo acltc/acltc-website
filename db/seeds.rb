caf922c94757a9ba6fd5df3bfbb11c93aea56856
dianacamacho <dcam1901@yahoo.com>
2016-09-06 14:39:30 -0500

    seed file for fields of interest

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+FieldOfInterest.create(name: "Ruby")
+FieldOfInterest.create(name: "Rails")
+FieldOfInterest.create(name: "Python")
+FieldOfInterest.create(name: "NodeJS")
+FieldOfInterest.create(name: "Angular")
+
+
