bd0b03ca890d2e33ac10e9b364ac89b0f96bfae6
dianacamacho <dcam1901@yahoo.com>
2016-09-12 11:16:52 -0500

    authentication for corp training messages

--- app/controllers/corporate_training_messages_controller.rb
+++ app/controllers/corporate_training_messages_controller.rb
@@ -1,6 +1,6 @@
 class CorporateTrainingMessagesController < ApplicationController
-  # before_action :authenticate_admin!, except: [:new, :create, :corporate_training]
-  # before_action :authenticate_corporate_training_access, only: [:index]
+  before_action :authenticate_admin!, except: [:new, :create, :corporate_training]
+  before_action :authenticate_corporate_training_access, only: [:index]
   invisible_captcha only: [:create], honeypot: :subtitle
   layout 'corporate_training_messages', except: [:corporate_training]
 
