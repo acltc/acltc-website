548c02149e47ef898dbb9e37175af4e655ea1099
heydanhey <danhey@gmail.com>
2016-10-11 08:45:03 -0500

    removed popup modal js

--- app/assets/javascripts/rebranding-modal.js
+++ app/assets/javascripts/rebranding-modal.js
@@ -1,12 +1,12 @@
-$(window).load(function(){
-    var value = Cookies.get('Actualize') || false;
-    if (!value){
-        $('#rebranding').modal('show');
-    }
-});
-$(document).ready(function(){
-    $('#rebranding').click(function(){
-        Cookies.set('Actualize', true, { expires: 7 });
-        $('#rebranding').modal('hide');
-    });
-});
+// $(window).load(function(){
+//     var value = Cookies.get('Actualize') || false;
+//     if (!value){
+//         $('#rebranding').modal('show');
+//     }
+// });
+// $(document).ready(function(){
+//     $('#rebranding').click(function(){
+//         Cookies.set('Actualize', true, { expires: 7 });
+//         $('#rebranding').modal('hide');
+//     });
+// });
