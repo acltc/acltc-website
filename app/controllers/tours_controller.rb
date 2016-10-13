0c983cb4ce802532ffc290d75b8f7677021f6af7
dianacamacho <dcam1901@yahoo.com>
2016-10-13 15:29:58 -0500

    tours controller, model methods, associations, index, show, edit, new

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,4 +1,27 @@
 class ToursController < ApplicationController
+  before_action :authenticate_admin!, only: [:index, :show, :edit, :update, :destroy]
+  before_action :set_tour, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tours = Tour.all
+  end
+
+  def index
+    @tours = Tour.all
+
+    if params[:location_filter]
+      @tours = Tour.where(city: params[:location_filter])
+      pp @tours
+    end
+
+    @tours = Tour.order(params[:sort]) if params[:sort]
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @tours.to_csv }
+    end
+  end
+
   def new
     @tour = Tour.new
   end
@@ -8,17 +31,54 @@ class ToursController < ApplicationController
 
     if @tour.save
       @tour.interview.update(booked: true)
-      AcltcMailer.application_email(@tour).deliver_now
-      redirect_to "/tours/thank_you"
+      AcltcMailer.tour_email(@tour).deliver_now
+      redirect_to tours_thank_you_path
     else
       render :new
     end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @initial_interview = @tour.interview
+
+    if @tour.update(tour_params)
+      if @tour.interview != @initial_interview
+        @initial_interview.update(booked: false)
+        @tour.interview.update(booked: true)
+      end
+    
+      flash[:info] = "Tour updated!"
+      redirect_to @tour
+    else
+      flash[:danger] = "Something went wrong!"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @tour.interview
+      @tour.interview.update(booked: false)
+    end
+    @tour.destroy
+
+    flash[:danger] = "Tour successfully deleted!"
+    redirect_to tours_path
   end
 
   def thank_you
   end
 
   private
+    def set_tour
+      @tour = Tour.find(params[:id])
+    end
+
     def tour_params
       params.require(:tour).permit(
         :city,
