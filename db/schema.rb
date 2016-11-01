db799423eebe87ae9715fc7200a038d00e302394
Justin Jungst <jjungstprogramming@gmail.com>
2016-11-01 11:25:22 -0500

    database migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013183356) do
+ActiveRecord::Schema.define(version: 20161101155447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -349,6 +349,7 @@ ActiveRecord::Schema.define(version: 20161013183356) do
     t.string   "city"
     t.string   "country"
     t.string   "postal_code"
+    t.string   "state"
   end
 
   create_table "tours", force: :cascade do |t|
