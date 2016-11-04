d4d83f50202bf0d4fb4614e596f41a71f142f9d8
Justin Jungst <jjungstprogramming@gmail.com>
2016-11-04 13:57:42 -0500

    apply works, the rest do not

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -9,103 +9,61 @@ class SubscribersController < ApplicationController
     end
   end
 
-  def create
-    client = Drip::Client.new do |c|
-      c.api_key = ENV["DRIP_CLIENT_API_TOKEN"]
-      c.account_id = ENV["DRIP_ACCOUNT_ID"]
-    end
+  def create_from_application
+    setup_subscriber
 
-    if params[:mousetrap] == "Application"
-      @subscriber = Subscriber.new(email: params[:email], first_name: params[:first_name], mousetrap: params[:mousetrap], ip_address: request.remote_ip)
-      if city = request.location.city
-        @subscriber.city = city
+    if @subscriber.save
+      subscriber_drip_setup
+      redirect_to "/applications/new/#{@subscriber.id}"
+    else
+      render :apply
+    end
+  end
+
+  def create_from_curriculum
+    if cookies[:is_subscriber]
+      respond_to do |format|
+        @java_url = "/subscribers/download"
+        format.js {render :partial => "downloadFile"}
       end
-      if state = request.location.state
-        @subscriber.state = state
-      end
-      if postal_code = request.location.postal_code
-        @subscriber.postal_code = postal_code
-      end
+    else
+      setup_subscriber
+
       if @subscriber.save
-        cookies.permanent[:is_subscriber] = true
-        client.create_or_update_subscriber(@subscriber.email)
-        AcltcMailer.subscriber_mousetrap_email(@subscriber).deliver_now
-        redirect_to "/applications/new/#{@subscriber.id}"
-      else
-        render :apply
-      end
-    elsif params[:mousetrap] == "Curriculum Download"
-      if cookies[:is_subscriber]
+        subscriber_drip_setup
         respond_to do |format|
           @java_url = "/subscribers/download"
           format.js {render :partial => "downloadFile"}
         end
-      else
-        @subscriber = Subscriber.new(email: params[:email], first_name: params[:first_name], mousetrap: params[:mousetrap], ip_address: request.remote_ip)
-        if city = request.location.city
-          @subscriber.city = city
-        end
-        if state = request.location.state
-          @subscriber.state = state
-        end
-        if postal_code = request.location.postal_code
-          @subscriber.postal_code = postal_code
-        end
-        if @subscriber.save
-          cookies.permanent[:is_subscriber] = true
-          client.create_or_update_subscriber(@subscriber.email)
-          AcltcMailer.subscriber_mousetrap_email(@subscriber).deliver_now
-          respond_to do |format|
-            @java_url = "/subscribers/download"
-            format.js {render :partial => "downloadFile"}
-          end
-        end
       end
-    elsif params[:mousetrap] == "View Tutorials"
-      if cookies[:is_subscriber]
-        tutorials_visible = true
-      else
-        @subscriber = Subscriber.new(email: params[:email], first_name: params[:first_name], mousetrap: params[:mousetrap], ip_address: request.remote_ip)
-        if city = request.location.city
-          @subscriber.city = city
-        end
-        if state = request.location.state
-          @subscriber.state = state
-        end
-        if postal_code = request.location.postal_code
-          @subscriber.postal_code = postal_code
-        end
-        if @subscriber.save
-          cookies.permanent[:is_subscriber] = true
-          client.create_or_update_subscriber(@subscriber.email)
-          AcltcMailer.subscriber_mousetrap_email(@subscriber).deliver_now
-          @tutorials_visible = true
-        else
-          @tutorials_visible = false
-        end
-        respond_to do |format|
-          format.js {render :partial => "viewTutorials"}
-        end
-      end
+    end
+  end
+
+  def create_from_tutorial
+    if cookies[:is_subscriber]
+      @tutorials_visible = true
     else
-      @subscriber = Subscriber.new(first_name: params[:firstname], email: params[:email], mousetrap: "Homepage Footer", ip_address: request.remote_ip) unless params[:email].blank?
-      if city = request.location.city
-        @subscriber.city = city
-      end
-      if state = request.location.state
-        @subscriber.state = state
-      end
-      if postal_code = request.location.postal_code
-        @subscriber.postal_code = postal_code
-      end
+      setup_subscriber
+
       if @subscriber.save
-        cookies.permanent[:is_subscriber] = true
-        client.create_or_update_subscriber(@subscriber.email)
-        AcltcMailer.subscriber_mousetrap_email(@subscriber).deliver_now
+        subscriber_drip_setup
         @tutorials_visible = true
       else
-        render :apply
+        @tutorials_visible = false
       end
+      respond_to do |format|
+        format.js {render :partial => "viewTutorials"}
+      end
+    end
+  end
+
+  def create_from_footer
+    setup_subscriber
+
+    if @subscriber.save
+      subscriber_drip_setup
+    else
+      render :apply
     end
   end
 
@@ -117,5 +75,31 @@ class SubscribersController < ApplicationController
     url = 'https://s3.amazonaws.com/acltc/Actualize_Curriculum_2016.pdf'
     data = open(url).read
     send_data data, :disposition => 'attachment', :filename=>"Actualize_Curriculum_2016.pdf"
+  end
+
+  private
+
+  def setup_subscriber
+    @subscriber = Subscriber.new(email: params[:email], first_name: params[:first_name], mousetrap: params[:mousetrap], ip_address: request.remote_ip)
+    if city = request.location.city
+      @subscriber.city = city
+    end
+    if state = request.location.state
+      @subscriber.state = state
+    end
+    if postal_code = request.location.postal_code
+      @subscriber.postal_code = postal_code
+    end
+  end
+
+  def subscriber_drip_setup
+    client = Drip::Client.new do |c|
+      c.api_key = ENV["DRIP_CLIENT_API_TOKEN"]
+      c.account_id = ENV["DRIP_ACCOUNT_ID"]
+    end
+
+    cookies.permanent[:is_subscriber] = true
+    client.create_or_update_subscriber(@subscriber.email)
+    AcltcMailer.subscriber_mousetrap_email(@subscriber).deliver_now
   end
 end
