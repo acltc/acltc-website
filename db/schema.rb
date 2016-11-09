d05b139e52686671740a195a64cae0f33d6a8d9e
Justin Jungst <jjungstprogramming@gmail.com>
2016-11-09 14:26:58 -0600

    added notes field to tours show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103184548) do
+ActiveRecord::Schema.define(version: 20161109165609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -361,6 +361,7 @@ ActiveRecord::Schema.define(version: 20161103184548) do
     t.integer  "interview_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.text     "notes"
   end
 
   create_table "tutorial_submissions", force: :cascade do |t|
