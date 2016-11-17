be2e3c2c8b68c7cc0fd9d050691d70ed2ce55fd0
Justin Jungst <jjungstprogramming@gmail.com>
2016-11-17 15:30:10 -0600

    added ben's email to lakewood recepients

--- app/mailers/acltc_mailer.rb
+++ app/mailers/acltc_mailer.rb
@@ -41,7 +41,7 @@ class AcltcMailer < ApplicationMailer
 
   def lakewood_application_email(application)
     @lakewood_application = application
-    mail(from: "sandboxd035c758dbb54757b1b13d910d57a6b3.mailgun.org", to: 'zev@actualize.co', subject: 'Lakewood Actualize Applicant', reply_to: @lakewood_application.email )
+    mail(from: "sandboxd035c758dbb54757b1b13d910d57a6b3.mailgun.org", to: ['zev@actualize.co', 'ben@thedifferenceengine.io'], subject: 'Lakewood Actualize Applicant', reply_to: @lakewood_application.email )
   end
 
   def mini_course_application_email(mini_course_application)
