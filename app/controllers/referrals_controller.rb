d61647c5df499927ecfaf6795a6bdd5b1b24e3a8
Daniel Chu <danchu123@gmail.com>
2017-02-02 13:04:45 -0600

    Structure of email template

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -7,19 +7,21 @@ class ReferralsController < ApplicationController
   end
 
   def create
-    @referral = Referral.new(
-        referrer_first_name: params[:referral][:referrer_first_name],
-        referrer_last_name: params[:referral][:referrer_last_name],
-        referrer_email: params[:referral][:referrer_email],
-        referrer_phone_number: params[:referral][:referrer_phone_number],
-        referred_first_name: params[:referral][:referred_first_name],
-        referred_email: params[:referral][:referred_email]
-      )
+    @referral = Referral.new(referral_params)
 
     if @referral.save
       redirect_to '/referrals/new'
     else
       render :new
     end
+  end
+
+  def thank_you
+  end
+
+  private
+
+  def referral_params
+    params.require(:referral).permit(:referrer_first_name, :referrer_last_name, :referrer_email, :referrer_phone_number, :referred_first_name, :referred_email)
   end
 end
