57a1aa43ecf61fc0b2af42e1fd1672249e25b8b5
dianacamacho <dcam1901@yahoo.com>
2017-02-03 15:21:38 -0600

    change application cohort select to use dynamic prework dates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,34 +1,21 @@
 class PagesController < ApplicationController
+  include CohortDatesHelper
   
   def home
     @info_session_sign_up = InfoSessionSignUp.new
     @current_info_session = InfoSession.current_info_session
 
-    cohort_dates = [
-      { prework: true, date: Date.new(2017, 1, 22) },
-      { prework: false, date: Date.new(2017, 2, 26) },
-      { prework: true, date: Date.new(2017, 3, 26) },
-      { prework: false, date: Date.new(2017, 4, 23) },
-      { prework: true, date: Date.new(2017, 5, 28) },
-      { prework: false, date: Date.new(2017, 6, 25) },
-      { prework: true, date: Date.new(2017, 7, 23) },
-      { prework: false, date: Date.new(2017, 8, 20) },
-      { prework: true, date: Date.new(2017, 9, 17) },
-      { prework: false, date: Date.new(2017, 10, 15) },
-      { prework: true, date: Date.new(2017, 11, 19) },
-      { prework: false, date: Date.new(2017, 12, 17) }
-    ].sort_by { |cohort_date| cohort_date[:date] }
+    cohort_dates = cohort_start_dates
 
     cohort_dates.each do |cohort_date|
       if Time.zone.now <= cohort_date[:date] - 1.week
-        @cohort_date = cohort_date[:date].strftime("%B %d, %Y")
+        @cohort_date = cohort_date[:date].strftime("%B %e, %Y")
         break
       else 
-        @cohort_date = cohort_dates.first[:date].strftime("%B %d, %Y")
+        @cohort_date = cohort_dates.first[:date].strftime("%B %e, %Y")
       end
     end
 
-   
     render :layout => 'home_application'
   end
 
