172cecdc78484e32cf5f2e2bcca07da9a2488532
Chris Connell <chris.g.connell@gmail.com>
2017-02-08 10:45:07 -0600

    sending created_at with pop-up subscriber

--- app/controllers/api/v1/subscribers_controller.rb
+++ app/controllers/api/v1/subscribers_controller.rb
@@ -44,7 +44,7 @@ class Api::V1::SubscribersController < ApplicationController
     def create_hubspot_contact(mousetrap_type)
       contact = Hubspot::Contact.find_by_email(@subscriber.email)
       if !contact
-        Hubspot::Contact.create!(@subscriber.email, {firstname: @subscriber.first_name, phone: @subscriber.phone, lead_type: "Mousetrap", mousetrap: mousetrap_type})
+        Hubspot::Contact.create!(@subscriber.email, {firstname: @subscriber.first_name, phone: @subscriber.phone, lead_type: "Mousetrap", mousetrap: mousetrap_type, created_at: @subscriber.created_at})
       end  
     end  
 end
