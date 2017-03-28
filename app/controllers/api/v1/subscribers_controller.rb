285f6e74dd4ab1c343f968c782e41e997629c1dd
Jay Wengrow <jaywngrw@gmail.com>
2017-03-27 20:45:46 -0500

    adding custom fields to drip

--- app/controllers/api/v1/subscribers_controller.rb
+++ app/controllers/api/v1/subscribers_controller.rb
@@ -36,7 +36,7 @@ class Api::V1::SubscribersController < ApplicationController
     end
 
     cookies.permanent[:is_subscriber] = true
-    client.create_or_update_subscriber(@subscriber.email, {first_name: @subscriber.first_name, phone: @subscriber.phone})
+    client.create_or_update_subscriber(@subscriber.email, {custom_fields: {first_name: @subscriber.first_name, phone: @subscriber.phone}})
     client.apply_tag(@subscriber.email, @subscriber.mousetrap)
     client.subscribe(@subscriber.email, 34197704)
     AcltcMailer.subscriber_mousetrap_email(@subscriber).deliver_now
