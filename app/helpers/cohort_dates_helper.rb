dd8a9c2e8a6691594c8c755a1775f4b7ef9032ff
Jay Wengrow <jaywngrw@gmail.com>
2017-10-11 15:57:33 -0500

    quick fix

--- app/helpers/cohort_dates_helper.rb
+++ app/helpers/cohort_dates_helper.rb
@@ -12,7 +12,7 @@ module CohortDatesHelper
       { prework: true, date: Date.new(2017, 10, 1), early_bird_date: nil },
       # { prework: false, date: Date.new(2017, 11, 5), early_bird_date: nil },
       # { prework: true, date: Date.new(2017, 12, 3), early_bird_date: Date.new(2017, 10, 31) },
-      { prework: false, date: Date.new(2018, 1, 7), early_bird_date: Date.new(2017, 10, 31) }
+      { prework: true, date: Date.new(2018, 1, 7), early_bird_date: Date.new(2017, 10, 31) }
     ].sort_by { |cohort_date| cohort_date[:date] }
   end
 
@@ -25,7 +25,7 @@ module CohortDatesHelper
       { prework: true, date: Date.new(2017, 10, 22), location: "east", early_bird_date: nil },
       # { prework: false, date: Date.new(2017, 11, 19), location: "east", early_bird_date: nil },
       # { prework: true, date: Date.new(2017, 12, 3), location: "west", early_bird_date: Date.new(2017, 10, 31) },
-      { prework: false, date: Date.new(2018, 1, 7), location: "west", early_bird_date: Date.new(2017, 10, 31) },
+      { prework: true, date: Date.new(2018, 1, 7), location: "west", early_bird_date: Date.new(2017, 10, 31) },
       { prework: true, date: Date.new(2018, 3, 11), location: "east", early_bird_date: nil },
       { prework: false, date: Date.new(2018, 4, 8), location: "east", early_bird_date: nil }
     ].sort_by { |cohort_date| cohort_date[:date] }
