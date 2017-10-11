4f302882ba77afc58e872d4f31e891ce388f5aaa
Jay Wengrow <jaywngrw@gmail.com>
2017-10-11 15:46:04 -0500

    trying new start date scheme

--- app/helpers/cohort_dates_helper.rb
+++ app/helpers/cohort_dates_helper.rb
@@ -10,9 +10,9 @@ module CohortDatesHelper
       { prework: true, date: Date.new(2017, 7, 23), early_bird_date: nil },
       { prework: false, date: Date.new(2017, 8, 20), early_bird_date: nil },
       { prework: true, date: Date.new(2017, 10, 1), early_bird_date: nil },
-      { prework: false, date: Date.new(2017, 11, 5), early_bird_date: nil },
-      { prework: true, date: Date.new(2017, 12, 3), early_bird_date: nil },
-      { prework: false, date: Date.new(2018, 1, 7), early_bird_date: nil }
+      # { prework: false, date: Date.new(2017, 11, 5), early_bird_date: nil },
+      # { prework: true, date: Date.new(2017, 12, 3), early_bird_date: Date.new(2017, 10, 31) },
+      { prework: false, date: Date.new(2018, 1, 7), early_bird_date: Date.new(2017, 10, 31) }
     ].sort_by { |cohort_date| cohort_date[:date] }
   end
 
@@ -23,9 +23,9 @@ module CohortDatesHelper
       { prework: true, date: Date.new(2017, 7, 23), location: "west", early_bird_date: nil },
       { prework: false, date: Date.new(2017, 8, 20), location: "west", early_bird_date: nil },
       { prework: true, date: Date.new(2017, 10, 22), location: "east", early_bird_date: nil },
-      { prework: false, date: Date.new(2017, 11, 19), location: "east", early_bird_date: nil },
-      { prework: true, date: Date.new(2017, 12, 3), location: "west", early_bird_date: Date.new(2017, 10, 31) },
-      { prework: false, date: Date.new(2018, 1, 7), location: "west", early_bird_date: nil },
+      # { prework: false, date: Date.new(2017, 11, 19), location: "east", early_bird_date: nil },
+      # { prework: true, date: Date.new(2017, 12, 3), location: "west", early_bird_date: Date.new(2017, 10, 31) },
+      { prework: false, date: Date.new(2018, 1, 7), location: "west", early_bird_date: Date.new(2017, 10, 31) },
       { prework: true, date: Date.new(2018, 3, 11), location: "east", early_bird_date: nil },
       { prework: false, date: Date.new(2018, 4, 8), location: "east", early_bird_date: nil }
     ].sort_by { |cohort_date| cohort_date[:date] }
