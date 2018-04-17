b7687b0b8980ab78dae4526e8a51efd6c64e05ef
Peter Jang <peter.x.jang@gmail.com>
2018-04-17 13:47:19 -0500

    Add calculations and content to comparisons page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -46,49 +46,65 @@ class PagesController < ApplicationController
       'boise-idaho' => {
         city: 'Boise',
         state: 'Idaho',
-        cost_of_living: 2000,
-        cost_of_relocation: 200,
-        average_salary: 40000,
-        cost_of_tuition: 15000,
+        average_monthly_living_cost: 2000,
+        average_yearly_salary: 40000,
         bootcamps: [
           {
-            name: 'Bloc',
-            price: 15000
+            name: 'General Assembly',
+            tuition: 13500,
+            weeks: 12
           },
           {
             name: 'Fullstack',
-            price: 17000
+            tuition: 15700,
+            weeks: 21
           },
           {
-            name: 'Boise Coders',
-            price: 12000
+            name: 'Hack Reactor Remote Beta',
+            tuition: 17800,
+            weeks: 12
           }
         ]
       },
-      'sacramento-california' => {
-        city: 'Sacramento',
-        state: 'California',
-        cost_of_living: 2000,
-        cost_of_relocation: 200,
-        average_salary: 40000,
-        cost_of_tuition: 15000,
+      'new-york-city' => {
+        city: 'New York City',
+        state: 'New York',
+        average_monthly_living_cost: 2000,
+        average_yearly_salary: 40000,
         bootcamps: [
           {
-            name: 'Bloc',
-            price: 15000
+            name: 'General Assembly',
+            tuition: 13500,
+            weeks: 12
           },
           {
-            name: 'Fullstack',
-            price: 17000
+            name: 'Flatiron School',
+            tuition: 15000,
+            weeks: 18
           },
           {
-            name: 'Boise Coders',
-            price: 12000
+            name: 'App Academy',
+            tuition: 16200,
+            weeks: 12
           }
         ]
       }
     }
-    @city_info = cities["#{params[:location]}"]
+    city_info = cities["#{params[:location]}"]
+    @city = city_info[:city]
+    @state = city_info[:state]
+    @competitors = city_info[:bootcamps]
+    @primary_competitor = @competitors[0]
+    @tuition_actualize = 9500
+    @tuition_competitor = @primary_competitor[:tuition]
+    @lost_salary_competitor = city_info[:average_yearly_salary] * @primary_competitor[:weeks] / 52.0
+    @relocation_cost_competitor = city_info[:average_monthly_living_cost] * 0.25 * @primary_competitor[:weeks]
+    percent_base = [@tuition_competitor, @lost_salary_competitor].max.to_f
+    @tuition_percent_actualize = (@tuition_actualize / percent_base) * 100
+    @tuition_percent_competitor = (@tuition_competitor / percent_base) * 100
+    @lost_salary_percent_competitor = (@lost_salary_competitor / percent_base) * 100
+    @relocation_cost_percent_competitor = (@relocation_cost_competitor / percent_base) * 100
+    @total_cost_competitor = @tuition_competitor + @lost_salary_competitor + @relocation_cost_competitor
     render layout: 'main'
   end
 
