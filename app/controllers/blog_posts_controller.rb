974e6cac0681c668abf810ce7a7050602106a865
Jay Wengrow <jaywngrw@gmail.com>
2018-05-12 21:59:09 -0500

    fixed bug and added new image

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -27,8 +27,7 @@ class BlogPostsController < ApplicationController
   end
 
   def create
-    blog_post = BlogPost.new(blog_post_params)
-    blog_post.published? = true
+    blog_post = BlogPost.new(blog_post_params.merge({published?: true}))
     if blog_post.save
       blog_post.blog_pics.each_with_index do |file, index|
         image_placeholder = "IMAGE#{index+1}"
