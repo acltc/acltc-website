23dcec96b1aad026b3aa4a3f49d0140796450c8b
Jay Wengrow <jaywngrw@gmail.com>
2018-06-04 15:32:48 -0500

    fix bug

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -115,37 +115,41 @@ class LessonsController < ApplicationController
   def show
     # Find the current lesson based on the parameterized url
     lesson_index = LESSONS.index { |lesson| params[:title] == lesson[:title].parameterize }
-    lesson = LESSONS[lesson_index]
-    @lesson_title = lesson[:title]
-    # Create the next and previous links
-    lesson_links = LESSONS.map do |each_lesson|
-      { text: each_lesson[:title], href: "/tutorial/lessons/#{each_lesson[:title].parameterize}" }
+    if !lesson_index
+        redirect_to "/"
+    else
+        lesson = LESSONS[lesson_index]
+        @lesson_title = lesson[:title]
+        # Create the next and previous links
+        lesson_links = LESSONS.map do |each_lesson|
+          { text: each_lesson[:title], href: "/tutorial/lessons/#{each_lesson[:title].parameterize}" }
+        end
+        index_link = { text: "All lessons", href: "/tutorial/lessons" }
+        lesson_links = [index_link] + lesson_links + [index_link]
+        @previous_link = lesson_links[1 + lesson_index - 1]
+        @next_link = lesson_links[1 + lesson_index + 1]
+        # Create the links to the remaining lessons in the season
+        # If it's the last episode in the season, create links for next season lessons
+        remaining_lessons = LESSONS[(lesson_index + 1)..-1].select do |each_lesson|
+          each_lesson[:category_id] == lesson[:category_id]
+        end
+        if remaining_lessons.length == 0
+          remaining_lessons = LESSONS[(lesson_index + 1)..-1].select do |each_lesson|
+            each_lesson[:category_id] == lesson[:category_id] + 1
+          end
+        end
+        @next_lessons = remaining_lessons.first(4).map do |each_lesson|
+          {
+            text: each_lesson[:title],
+            href: each_lesson[:title].parameterize,
+            time: each_lesson[:time],
+            time_mac: each_lesson[:time_mac],
+            time_win: each_lesson[:time_win],
+            icon: CATEGORIES[each_lesson[:category_id]][:icon]
+          }
+        end
+        category_path = CATEGORIES[lesson[:category_id]][:title].parameterize
+        render "/lessons/#{category_path}/#{params[:title]}"
     end
-    index_link = { text: "All lessons", href: "/tutorial/lessons" }
-    lesson_links = [index_link] + lesson_links + [index_link]
-    @previous_link = lesson_links[1 + lesson_index - 1]
-    @next_link = lesson_links[1 + lesson_index + 1]
-    # Create the links to the remaining lessons in the season
-    # If it's the last episode in the season, create links for next season lessons
-    remaining_lessons = LESSONS[(lesson_index + 1)..-1].select do |each_lesson|
-      each_lesson[:category_id] == lesson[:category_id]
-    end
-    if remaining_lessons.length == 0
-      remaining_lessons = LESSONS[(lesson_index + 1)..-1].select do |each_lesson|
-        each_lesson[:category_id] == lesson[:category_id] + 1
-      end
-    end
-    @next_lessons = remaining_lessons.first(4).map do |each_lesson|
-      {
-        text: each_lesson[:title],
-        href: each_lesson[:title].parameterize,
-        time: each_lesson[:time],
-        time_mac: each_lesson[:time_mac],
-        time_win: each_lesson[:time_win],
-        icon: CATEGORIES[each_lesson[:category_id]][:icon]
-      }
-    end
-    category_path = CATEGORIES[lesson[:category_id]][:title].parameterize
-    render "/lessons/#{category_path}/#{params[:title]}"
   end
 end
