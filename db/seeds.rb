1c8fe47efef29783016b3543525912bd8d43f392
Jay Wengrow <jaywngrw@gmail.com>
2019-05-12 19:41:47 -0500

    seeds file for tlase videos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,39 @@
-FieldOfInterest.create(name: "Ruby")
-FieldOfInterest.create(name: "Rails")
-FieldOfInterest.create(name: "Python")
-FieldOfInterest.create(name: "NodeJS")
-FieldOfInterest.create(name: "AngularJS")
-FieldOfInterest.create(name: "Express.js")
-FieldOfInterest.create(name: "JavaScript")
-FieldOfInterest.create(name: "JQuery")
-FieldOfInterest.create(name: "HTML")
-FieldOfInterest.create(name: "CSS")
-FieldOfInterest.create(name: "MongoDB")
-FieldOfInterest.create(name: "Git")
+TlaseVideo.create(episode: 1, title: "Thinking About Time Complexity", description: "You can write code in two different ways to accomplish the same thing, yet one version is faster than the other. Learn how to begin to analyze the speed of your code.", vimeo_url: "335458912")
+
+TlaseVideo.create(episode: 2, title: "Dealing with Tricky Errors", description: "Many times, error messages pinpoint the exact line of code that is buggy. Sometimes, though, an error message may seem to pick on a line that is perfectly valid. Learn how to understand what these tricky error messages are telling you.", vimeo_url: "335459005")
+
+TlaseVideo.create(episode: 3, title: "Getting to the Bottom of Things", description: "My friend was asked this problem when interviewing for a software engineering role at Google. Would you have been able to solve it? Learn a special technique for analyzing some of the trickiest questions.", vimeo_url: "335459394")
+
+TlaseVideo.create(episode: 4, title: "English Code Isn’t English", description: "Code doesn't always translate into English precisely the way you think it might. By understanding how the computer actually understands your code, you'll be able to write better code.", vimeo_url: "335459683")
+
+TlaseVideo.create(episode: 5, title: " How Arrays Work", description: "Understanding how a computer's memory works can totally flip the way you understand the efficiency of your code. This is key to successful software engineering.", vimeo_url: "335459752")
+
+TlaseVideo.create(episode: 6, title: "The Constraints of the Computer", description: "Here I use a pattern known as \"array mapping\" to discuss what is probably the most important aspect of computer programming - the ability to talk to a computer. Being able to talk to a computer requires you to know what the computer understands, and what it doesn't understand.", vimeo_url: "335457755")
+
+TlaseVideo.create(episode: 7, title: "Artificial Intelligence", description: "Did you ever wonder how artificial intelligence works? AI is a very broad topic, but in this video, I give you a glimpse into how a software engineer can get a computer to mimic human intelligence.", vimeo_url: "335458192")
+
+TlaseVideo.create(episode: 8, title: "Hacking the Paywall", description: "Here, we hack someone else's website to get past an annoying paywall. (For educational purposes only.)", vimeo_url: "335458274")
+
+TlaseVideo.create(episode: 9, title: "What on Earth is Wrong?!", description: "You probably know that attention to detail is really important for a software engineer. But perhaps you don't realize to what extreme...", vimeo_url: "335458780")
+
+TlaseVideo.create(episode: 10, title: "Learning Rust Part #1", description: "In which I - unscripted - attempt to teach myself a brand new programming language on the spot. Do I succeed?", vimeo_url: "335456990")
+
+TlaseVideo.create(episode: 11, title: "Learning Rust Part #2", description: "This is the continuation of my previous week's attempt to teach myself a new programming language on the spot.", vimeo_url: "335464332")
+
+TlaseVideo.create(episode: 12, title: " How to Find Stuff", description: "When dealing with lots of code, how do you remember what you put where? Even tougher, when dealing with someone else's code, how do you figure out where they put everything? You can actually find anything you need with this one, weird trick. Actually, it's not weird at all. But it's really simple!", vimeo_url: "335464756")
+
+TlaseVideo.create(episode: 13, title: "Debugging a Real Website Error", description: "I received an alert that there was an error on my website. So instead of just fixing it, I recorded myself fixing it so you can see for yourself how a software engineer fixes real bugs!", vimeo_url: "335465279")
+
+TlaseVideo.create(episode: 14, title: "How to Isolate the Problem", description: "When something goes wrong, isolating the problem is one of the most important techniques you'll need if you want to fix it. In this video, we use a powerful analogy from IT that will help you learn to systematically isolate any bug.", vimeo_url: "335466025")
+
+TlaseVideo.create(episode: 15, title: "Automate Everyday Tasks Without Code", description: "With the software engineer mindset, you can actually automate day-to-day tasks without even using code. Watch how I automatically populate a Google spreadsheet in real-time with Slack posts that I want to save for later. It's fun, and something that you can get started with right away even if you don't know how to code (yet).", vimeo_url: "335462540")
+
+TlaseVideo.create(episode: 16, title: "How to Use Stack Overflow", description: "If you've ever Googled for coding help, you've probably found the Stack Overflow website. But sometimes, there are tons of answers to a given question! How do you know which one is right?", vimeo_url: "335462911")
+
+TlaseVideo.create(episode: 17, title: "Reverse-Engineering Documentation", description: "There's a trick that experienced software engineers use when reading technical documentation, and it can save you hours of time.", vimeo_url: "335461172")
+
+TlaseVideo.create(episode: 18, title: "Installing Stuff!", description: "I don't know about you, but when I first started out coding, I had a terribly difficult time downloading programming languages and tools. I've found that so many people are deterred from learning to code simply because they can't get past the installation process! But if you know what you're doing, installing stuff doesn't have to be that hard. (Most of the time, anyway...)", vimeo_url: "335461773")
+
+TlaseVideo.create(episode: 19, title: "Gremlins!", description: "Is it possible to have a bug in your code that has no logical reason? Maybe something's just wrong with your computer? Let’s have a conversation about gremlins.", vimeo_url: "335460987")
 
 
